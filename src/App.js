3331fac682eb9b0674a291891565d4fa60cc7e8b
FlamerFuel <areschoug@gmail.com>
2020-01-17 08:46:39 +0100

    Done: App.js under constuction

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import UnderContruction from './components/underContruction';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="main-header">
+				<h1>CityPop</h1>
+			</header>
+			<main>
+				<UnderContruction />
+			</main>
+		</div>
+	);
 }
 
 export default App;
