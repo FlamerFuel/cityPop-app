7fd85adea8d84e32086a0e8d6ed25f52b3c02c69
FlamerFuel <areschoug@gmail.com>
2020-01-20 16:41:55 +0100

    geoNames API in single search ready to merge and launch V.2

---
+++ search.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function search() {
+	return (
+		<section className="searchBox-wrap">
+			<input
+				type="text"
+				placeholder="Search for a city..."
+				className="searchBox"
+			/>
+		</section>
+	);
+}
+
+export default search;
